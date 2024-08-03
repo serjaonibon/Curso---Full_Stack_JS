12ee02c7cfbd834563046dd05a2787cb8fd1fefc
Sergio Nibon <snibonjr@gmail.com>
2024-08-02 22:02:49 -0300

    atualização

---
+++ one-bit-code-JavaScript/aula-25-Estruturas-de-Repetição-Do-While/do-while.js
@@ -0,0 +1,8 @@
+let velocidade = 100
+
+do {
+    alert("A velocidade do veículo é " + velocidade + "km/")
+    velocidade -= 20
+} while (velocidade >0)
+
+alert("Velocidade final: " + velocidade + " km/h")
---
+++ one-bit-code-JavaScript/exercicio-5/financas.js
@@ -0,0 +1,27 @@
+let saldo = prompt("Informe a quantidade de dinheiro inicial: ");
+saldo = parseFloat(saldo)
+let opcao = ""
+
+do {
+  opcao = prompt(
+    "Saldo disponível: R$ " +  saldo + "\n" +
+    "1. Adicionar dinheiro " + "\n" +
+    "2. Remover dinheiro" + "\n" +
+    "3. Sair" + "\n"
+  )
+
+  switch (opcao) {
+    case "1":
+        saldo += parseFloat(prompt("Informe o valor a ser adicionado:"))
+        break
+    case "2":
+        saldo -= prompt("Informe o valor a ser removido:")
+        break
+    case "3":
+        alert("Saindo ...")
+        break
+    default:
+        alert("Entrada inválida!")
+  }
+
+} while (opcao !== "3");
