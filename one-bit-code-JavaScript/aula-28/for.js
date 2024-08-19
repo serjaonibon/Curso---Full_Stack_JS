644883dbeeda382ef753ad6fa8f3d47380b172d4
Sergio Nibon <snibonjr@gmail.com>
2024-08-19 19:08:19 -0300

    Atualização em 19/08/2024

--- one-bit-code-JavaScript/aula-28/for.js
+++ one-bit-code-JavaScript/aula-28/for.js
@@ -1,3 +1,13 @@
-for (let indice = 0; indice <=10; indice++) {
+/*
+
+// inicialização ; condição ; finalização
+for (let indice = 0; indice <= 11; indice++) {
     alert(" Indice = " + indice)
+}
+
+*/
+
+let nome = "Nibon"
+for(let indice = 0; indice <= nome.length; indice++) {
+    console.log(nome[indice])
 }
---
+++ one-bit-code-JavaScript/exercicio-6/tabuada.js
@@ -0,0 +1,12 @@
+const numero = prompt(
+    "Olá, eu sou o robô da tabuada!" + "\n" +
+    "Informe o número que você deseja calcular a tabuada: "
+)
+
+let resultado = ""
+
+for(fator = 1; fator <= 20; fator++) {
+    resultado += " -> " + numero + " + " + fator + " = " + (numero * fator) + "\n"
+}
+
+alert("Resultado da tabuada de " + numero + ":\n\n" + resultado)
