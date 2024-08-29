e85f73841dfa82b40e3f2304f96f355f31a3ed90
Sergio Nibon <snibonjr@gmail.com>
2024-08-28 21:26:11 -0300

    Atualização em 28/08/2024

--- one-bit-code-JavaScript/aula-42/parametros.js
+++ one-bit-code-JavaScript/aula-42/parametros.js
@@ -1,40 +1,57 @@
-function dobro(x) {
-  alert("O dobro de " + x + " é " + x * 2);
+
+
+
+/*
+
+function dizerOla(nome) {
+  alert("Olá, " + nome + "!")
 }
 
-// dobro(5)
+ // dizerOla("Serjão")
 
-function dizerOla(nome = "Mundo") {
-  alert("Olá, " + nome + "!");
+ function soma (a, b) {
+  alert("O resultado da soma é " + (a + b))
+ }
+
+// soma(7, 3)
+
+function criarUsuario (nome, email, senha, tipo = "admin") {
+  const usuario =  {
+    nome, // mesmo que -> nome: nome, 
+    email, // mesmo que -> email: email,
+    senha, // mesmo que -> senha: senha,
+    tipo // mesmo que -> tipo: tipo
+
+  }
+  console.log(usuario)
 }
 
-// dizerOla("Serjão")
-// dizerOla()
+criarUsuario("Nibon", "snibonjr@gmail.com", "1234")
 
-function soma(a, b, c, d, e) {
-  alert("O resultado da soma é " + (a + b + c + d + e));
+*/
+
+/*
+
+function muitosParametros (nome, telefone, endereco, aniversario, email, senha) {
+  
 }
 
-// soma(1, 5, 2, 3, 10)
-
-function criarUsuario(nome, email, senha, tipo = "admin") {
-  const usuario = {
-    nome, // mesmo que  " nome: nome"
-    email, // mesmo que  " email: email"
-    senha, // mesmo que  " senha: senha"
-    tipo // mesmo que  " tipo: tipo"
-  };
-  console.log(usuario);
+function objetoComoParametro (usuario) {
+  
 }
 
+muitosParametros("nome", "telefone", "endereco", "aniversario", "email", "senha", )
 
-function muitosParametros(nome, telefone, endereco, aniversario, email, senha) {
-    
+
+*/
+
+const dadosDoUsuario = {
+  nome: "Sergio Nibon",
+  telefone: "85 99101-9273",
+  email: "snibonjr@gmail.com",
+  senha: "170829",
+  endereco: "Av. Francisco Sá",
+  aniversario: "17/02/1979"
 }
-muitosParametros(nome, telefone, endereco, aniversario, email, senha)
 
-
-function objetoComoParametro(nome, telefone, endereco, aniversario, email, senha) {
-    
-}
-
+console.log(dadosDoUsuario)
