f44418d47acc6b823e19573a8ec680c0d75aa33a
Sergio Nibon <snibonjr@gmail.com>
2024-09-04 07:09:56 -0300

    Atualização em 04/09/2024

---
+++ one-bit-code-JavaScript/aula-51/high-order-function-e-arrays.js
@@ -0,0 +1,16 @@
+// High-Order Functions e Arrays - Parte 1
+
+// Array usado nos exemplos
+
+const personagens = [
+    { nivel: 42, nome: "Thrall", raca: "Orc", classe: "Xamã" },
+    { nivel: 28, nome: "Garrosh", raca: "Orc", classe: "Guerreiro" },
+    { nivel: 35, nome: "Varok", raca: "Orc", classe: "Guerreiro" },
+    { nivel: 35, nome: "Uther", raca: "Humano", classe: "Paladino" },
+    { nivel: 26, nome: "Jaina", raca: "Humano", classe: "Maga" },
+    { nivel: 39, nome: "Tyrande", raca: "Elfo Noturno", classe: "Sacerdotisa" },
+    { nivel: 29, nome: "Muradin", raca: "Anão", classe: "Guerreiro" },
+  ]
+
+
+  
