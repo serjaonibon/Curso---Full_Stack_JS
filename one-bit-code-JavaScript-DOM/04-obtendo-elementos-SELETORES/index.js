0d447eb5c1d2a43991a4498159688cb929212b15
Sergio Nibon <snibonjr@gmail.com>
2024-09-21 11:02:25 -0300

    Atualização em 21/09/2024

--- one-bit-code-JavaScript-DOM/04-obtendo-elementos-SELETORES/index.js
+++ one-bit-code-JavaScript-DOM/04-obtendo-elementos-SELETORES/index.js
@@ -1,25 +1,23 @@
-// No javascript, através da função show() que será executada 
+// No javascript, através da função show() que será executada
 //ao clicar no botão, podemos obter um elemento do DOM através do id:
 
 function show() {
-    // Obter um elemento único a partir do seu id
-    const contactList = document.getElementById("contact-list")
-    console.log(contactList)
+  // Obter um elemento único a partir do seu id
+  const contactList = document.getElementById("contact-list");
+  console.log(contactList);
 
-    const listElements = document.getElementsByTagName("li")
-    console.log(listElements)
+  const listElements = document.getElementsByTagName("li");
+  console.log(listElements);
 
-    const contactInputs = document.getElementsByClassName("contact-input")
-    console.log(contactInputs)
+  const contactInputs = document.getElementsByClassName("contact-input");
+  console.log(contactInputs);
 
-    const contacts = document.querySelectorAll("#contact-list > li > label")
-    console.log(contacts)
+  const contacts = document.querySelectorAll("#contact-list > li > label");
+  console.log(contacts);
 
-    const contact1 = document.getElementsByTagName("contact1")
-    console.log(contact1)
+  const contact1 = document.getElementsByTagName("contact1");
+  console.log(contact1);
 
-    const firstContact = document.querySelector("#contact-list > li > label")
-    console.log(firstContact)
-    
-
-  }
+  const firstContact = document.querySelector("#contact-list > li > label")
+  console.log(firstContact)
+}
--- one-bit-code-JavaScript-DOM/05-criando-elementos/index.js
+++ one-bit-code-JavaScript-DOM/05-criando-elementos/index.js
@@ -1,14 +1,16 @@
 function addInput() {
     const ul = document.getElementById('inputs')
   
-    const newLi = document.createElement('li')
+    const newLi = document.createElement('li') // Estamos informando a tag que irá ser craiada "li"
     newLi.className = 'list-item'
     newLi.innerText = 'Novo input: '
   
-    const newInput = document.createElement('input')
+    const newInput = document.createElement('input')// criado um input para ser associado ao "li" criado anteriormente
     newInput.type = 'text'
     newInput.name = 'input'
   
     newLi.appendChild(newInput)
     ul.appendChild(newLi)
+
+
   }
